24e962d6049bef4340a67bf87706e071213fe1cb
LewisParr <lewisparr@gmail.com>
2022-07-11 15:51:48 +0100

    Added InternalUserController

---
+++ app/Http/Controllers/InternalUserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\InternalUser;
+
+class InternalUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return  \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get all internal users
+        return InternalUser::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param   \Illuminate\Http\Request    $request
+     * @return  \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // create an internal user
+        return InternalUser::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param   int $id
+     * @return  \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // show an internal user
+        return InternalUser::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param   \Illuminate\Http\Request    $request
+     * @param   int                         $id
+     * @return  \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // update an internal user
+        $user = InternalUser::find($id);
+        $user->update($request->all());
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param   int $id
+     * @return  \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // delete an internal user
+        return InternalUser::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use App\InternalUser;
 */
 
 Route::resource('user', 'UserController');
+Route::resource('internal_user', 'InternalUserController');
 
 /*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
